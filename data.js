306014a3eb5ffc7b4d915a41c8299c8afe2fed10
Marco Vandone <servizi.marco@gmail.com>
2026-06-17 14:50:59 +0200

    Initialize data structure for personal portfolio

---
+++ data.js
@@ -0,0 +1,81 @@
+const SITE = {
+    name: "YOUR_NAME",
+    title: "YOUR_POSITION",
+    email: "you@email.com",
+    github: "#",
+    linkedin: "#",
+    scholar: "#",
+    cv: "assets/cv.pdf",
+
+    about: `
+        Write your biography here.
+
+        Add multiple paragraphs if needed.
+    `
+};
+
+const EDUCATION = [
+{
+    title: "PhD in Computer Science",
+    organization: "University Name",
+    years: "2024 - Present",
+    description: "Research description"
+},
+{
+    title: "Research Intern",
+    organization: "Company",
+    years: "2023",
+    description: "Worked on..."
+}
+];
+
+const AWARDS = [
+{
+    name: "Best Paper Award",
+    year: "2025"
+}
+];
+
+const PROJECTS = [
+{
+    title: "Project Name",
+    image: "images/projects/project1.jpg",
+    description: "Description",
+    github: "#",
+    website: "#"
+}
+];
+
+const NEWS = [
+{
+    date: "Jun 2026",
+    text: "Paper accepted at CVPR."
+},
+{
+    date: "Feb 2026",
+    text: "Released project."
+}
+];
+
+const PUBLICATIONS = [
+{
+    year: 2026,
+    image: "images/publications/pub1.jpg",
+    title: "Paper Title",
+    authors: "Author A, Author B",
+    venue: "CVPR 2026",
+    paper: "#",
+    code: "#",
+    project: "#"
+},
+{
+    year: 2025,
+    image: "images/publications/pub2.jpg",
+    title: "Paper Title 2",
+    authors: "Author A, Author B",
+    venue: "NeurIPS 2025",
+    paper: "#",
+    code: "#",
+    project: "#"
+}
+];
